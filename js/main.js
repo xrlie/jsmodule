9498f60ea5473cd445a646918eed65593ae9321a
xrlie <c.aviles.rz@gmail.com>
2020-12-07 21:12:43 -0600

    Agregué el código de la clase uno

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+console.log('Hola Koders')
+
+// variables: var, let, const
+var nombre_variable; // Esta forma no se va a utilizar mucho
+let nombre_variable2 // Inicializa una variable vacía
+
+let nombre_variable3 = 'Algo'
+let numero = 1000
+let boolean = false
+let my_array = [1, '1', true] // Un array puede tener valores de diferente tipos
+let my_object_koder = {
+    nombre: 'Carlos'
+}
+
+// let, se utiliza para variables cuyo valor cambia
+// const, se utiliza para variables cuyo valor NO cambia
+const NAME_KODER = 'oscar'
+let name_koder2 = 'oscar'
