749e7719ee92f8191bf388ef66e800d66f6c4932
xrlie <c.aviles.rz@gmail.com>
2020-12-08 21:57:28 -0600

    ADD main if ternario

--- js/main.js
+++ js/main.js
@@ -3,16 +3,31 @@ console.log('Condicionales')
 /* 
 
 if (condición) {
-
+    si se cumple el if, se ejecuta el código de aquí
 }
 
 else (condición) {
-
+    
 }
 
 */
 
+//=====================================================//
 
+// IF Ternario
+
+// expresión ? si true : si false
+
+const NUMBER_ONE = parseInt(prompt('Ingresa un número'))
+const NUMBER_TWO = parseInt(prompt('Ingresa un segundo número'))
+
+NUMBER_ONE > NUMBER_TWO ? 
+    console.log(`El ${NUMBER_ONE} es el mayor`)
+    : 
+    console.log(`El ${NUMBER_TWO} es el mayor`)
+
+
+//=====================================================//
 /*
 
 Operadores de asignación
