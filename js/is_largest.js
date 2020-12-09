69deddea56b1054732f5690d3b24045d7ab147e7
xrlie <c.aviles.rz@gmail.com>
2020-12-08 20:38:43 -0600

    ADD is_largest.js

---
+++ js/is_largest.js
@@ -0,0 +1,19 @@
+// Ejercicio
+// 1. Estar en la rama de la clase.
+// 2. Crear el archivo is_largest.js
+// 3. Pedir al usuario que ingrese 2 números.
+// 4. Imprimir en consola el mayor
+
+console.log('is largest')
+
+const NUMBER_ONE = parseInt(prompt('Ingresa un número:'))
+const NUMBER_TWO = parseInt(prompt('Ingresa un número:'))
+
+if (NUMBER_ONE > NUMBER_TWO) {
+    console.log(`El ${NUMBER_ONE} es el mayor`)
+} else if (NUMBER_ONE === NUMBER_TWO) {
+    console.log(`El ${NUMBER_ONE} es igual al ${NUMBER_TWO}`)
+}
+else {
+    console.log(`El ${NUMBER_TWO} es el mayor`)
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log('Hola Koders')
+console.log('Condicionales')
+
+/* 
+
+if (condición) {
+
+}
+
+else (condición) {
+
+}
+
+*/
+
+
+/*
+
+Operadores de asignación
+
+let age = 10
+//age = age + 1
+age += 1
+console.log(age)
+
+*/
