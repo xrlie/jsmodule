f65da0239d9828521a32524fb655647ac024f9f7
xrlie <c.aviles.rz@gmail.com>
2020-12-14 21:19:48 -0600

    ADD arrow functions

--- js/arrow_functions.js
+++ js/arrow_functions.js
@@ -22,3 +22,50 @@ const add_arrow = (a,b) => {
 
 add_arrow(2, 3)
 
+ /* * * * * * * * * * * *
+  *  Arrow Function 1   *
+  * * * * * * * * * * * */
+
+const calculate_dog_age = (dog_name, human_age) => { human_age * 7 }
+
+ /* * * * * * * * * * * *
+  *  Arrow Function 2   *
+  * * * * * * * * * * * */
+
+const calc_circumference = (radius) => { 3.1416 * 2 * radius }
+
+const calc_area = (radius) => { 3.1416 * Math.pow(radius,2) }
+
+ /* * * * * * * * * * * *
+  *  Arrow Function 3   *
+  * * * * * * * * * * * */
+
+const temperature_converter = (degrees, units) => {
+    switch (units) {
+        case('C'):
+        case('c'):
+           let degrees_fahrenheit = (degrees * 1.8 + 32).toFixed(0)
+           return degrees_fahrenheit
+           //break;
+        case('F'):
+        case('f'):
+           let degrees_celcius = ((degrees - 32) / 1.8).toFixed(0)
+           return degrees_celcius
+           //break;
+        default:
+            console.log("Ingresa un tipo de unidad correcta: C o F");
+            break;
+    }
+}
+
+ /* * * * * * * * * * * *
+  *  Arrow Function 4   *
+  * * * * * * * * * * * */
+
+const get_factorial = (number) => {
+    let factorial = 1
+    for(let i = number; i > 0; --i) {
+        factorial *= i
+    }
+    return factorial
+}
