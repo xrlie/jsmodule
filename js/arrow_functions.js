1f9214c179f6c4a29f4b50c4b523508e84563bc0
xrlie <c.aviles.rz@gmail.com>
2020-12-14 20:33:59 -0600

    ADD arrow functions file

---
+++ js/arrow_functions.js
@@ -0,0 +1,24 @@
+/* * * * * * * * * *
+ * Arrow Functions *
+ * * * * * * * * * */
+
+/*
+function name_function (par1, par2,) {
+    // sentencias 
+}
+
+const name_function = (par1, par2) => {
+    // sentencias
+}
+*/
+
+function add (a,b) {
+    return a + b
+}
+
+const add_arrow = (a,b) => {
+    return a + b
+}
+
+add_arrow(2, 3)
+
