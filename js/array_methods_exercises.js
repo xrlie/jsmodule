407ae4811bb4a4da8d82207ea575bf8371b560a3
xrlie <c.aviles.rz@gmail.com>
2020-12-17 17:54:39 -0600

    ADD ejercicios resueltos métodos array

--- js/array_methods_exercises.js
+++ js/array_methods_exercises.js
@@ -56,6 +56,19 @@ const capitalize = rand_array => {
 // [1,2,4,2,5,4]
 // -> [1,5]
 
+const no_repeated_num = num_array => {
+    return new_array = num_array.filter(element => {
+        let count_aux = 0
+        for (let i = 0; i < num_array.length; ++i) {
+            if (element === num_array[i]) {
+                ++count_aux
+            }
+        }
+        if (count_aux < 2) {
+            return element
+        }
+    })
+}
 
 
 // ejercicio 5 .filter()
@@ -65,7 +78,54 @@ const capitalize = rand_array => {
 // [1,2,4,2,5,4]
 // -> [2,4]
 
+// const repeated_num = num_array => {
+//     let aux = 0
+//     return new_array = num_array.filter(element => {
+//         console.log(new_array.length)
+//         let count_aux = 0
+//         for (let i = 0; i < num_array.length; ++i) {
+//             if (element === num_array[i]) {
+//                 ++count_aux
+//             }
+//         }
+//         if (count_aux > 1) {
+//             ++aux
+//             if (aux <2) {
+//                 return element
+//             }
+//         }
+//     })
+// }
 
+// // const repeated_num = num_array => {
+// //     let new_array = [] 
+// //     aux_array = num_array.filter(element => {
+// //         let aux = 0
+// //         let count_aux = 0
+// //         for (let i = 0; i < num_array.length; ++i) {
+// //             if (element === num_array[i]) {
+// //                 ++count_aux
+// //             }
+// //         }
+// //         if (count_aux > 1) {
+// //             ++aux
+// //             if (aux <2) {
+// //                 return element
+// //             }
+// //         }
+// //     })
+
+// //     new_array = aux_array.filter((element, index) => {
+// //         let count_aux = 0
+// //         for (let i = 0; i < aux_array.length; ++i) {
+// //             if (element === aux_array[i]) {
+// //                 ++count_aux
+// //             }
+// //             if ()
+// //         }
+// //     })
+// //     return new_array
+// // }
 
 // ejercicio 6 .filter()
 //  funcion
@@ -73,3 +133,15 @@ const capitalize = rand_array => {
 // y devuelva un array con los años que son bisiesto
 // [1990, 2000, 2001, 2020]
 // -> [2000,2020]
+
+const is_leap_year = years_array => {
+    return leap_years = years_array.filter(year => {
+        if(year % 4 === 0) {
+            if(year % 100 === 0) {
+                if (year % 400 === 0) {
+                    return year
+                }
+            } else { return year}
+        }
+    })
+}
